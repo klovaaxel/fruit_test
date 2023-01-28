16169cba106eaf2e002269666da6a437c4850ee1
klovaaxel <klovakarlsson@gmail.com>
2023-01-28 19:16:58 +0100

    Add scope to servicworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,17 +1,19 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-        navigator.serviceWorker.register("serviceworker.js").then(
-            function (registration) {
-                // Registration was successful
-                console.log(
-                    "ServiceWorker registration successful with scope: ",
-                    registration.scope
-                );
-            },
-            function (err) {
-                // registration failed :(
-                console.log("ServiceWorker registration failed: ", err);
-            }
-        );
+        navigator.serviceWorker
+            .register("serviceworker.js", { scope: "." })
+            .then(
+                function (registration) {
+                    // Registration was successful
+                    console.log(
+                        "ServiceWorker registration successful with scope: ",
+                        registration.scope
+                    );
+                },
+                function (err) {
+                    // registration failed :(
+                    console.log("ServiceWorker registration failed: ", err);
+                }
+            );
     });
 }
